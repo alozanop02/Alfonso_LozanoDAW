b99a865d0e2cdd53a521e88cf28505bbfb67ffb4
alozanop02 <alozanop02@iesalbarregas.es>
2021-03-18 19:04:22 +0100

    "Líneas en método main"

--- src/Principal.java
+++ src/Principal.java
@@ -4,8 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Tarea 6 Tema 4");
-		System.out.println("Primera línea añadida");
-		System.out.println("Segunda línea añadida");
+		System.out.println("Primera línea añadida.");
+		System.out.println("Segunda línea añadida.");
 	}
 
 }
